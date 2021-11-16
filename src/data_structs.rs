e7022a4e48d6dd6880c58559c64de35e01fb3149
S0raWasTaken <anosensedroi@gmail.com>
2021-11-16 14:33:34 -0500

    Begin constructing the base for thread management

--- src/data_structs.rs
+++ src/data_structs.rs
@@ -1,10 +1,37 @@
 use std::collections::BTreeMap;
 
 use rustbreak::{backend::FileBackend, deser::Yaml, Database};
+use serde::{Deserialize, Serialize};
 use serenity::prelude::TypeMapKey;
 
 pub struct Prefixes;
+pub struct MediaChannel;
+
+// TODO
+// pub struct ThreadOptions {}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct ChannelOptions {
+    mod_talk: bool,
+    admin_talk: bool,
+    member_talk: bool,
+}
+
+#[allow(dead_code)]
+impl ChannelOptions {
+    pub fn new() -> Self {
+        Self {
+            mod_talk: false,
+            admin_talk: false,
+            member_talk: false,
+        }
+    }
+}
 
 impl TypeMapKey for Prefixes {
     type Value = Database<BTreeMap<u64, String>, FileBackend, Yaml>;
+}
+
+impl TypeMapKey for MediaChannel {
+    type Value = Database<BTreeMap<u64, ChannelOptions>, FileBackend, Yaml>;
 }
